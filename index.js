60c42dc95dcc6716762f267193d4d36be68ea6c3
Ethan Tyler <38672607+zonedv2@users.noreply.github.com>
2021-12-22 00:49:42 +0000

    authentication implemented

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const Pool = require('pg').Pool
 const bcrypt = require('bcrypt')
+const dotenv = require('dotenv').config()
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
@@ -10,9 +11,12 @@ const pool = new Pool({
   password: 'ZonedOnline123',
   port: 5432,
 })
+const jwt = require('jsonwebtoken')
 
 app.use(cors())
 app.use(express.json())
+
+console.log(process.env.SECRET)
 
 app.post('/api/users/login', async (request, response) => {
     pool.query('SELECT * FROM users WHERE email = $1', [request.body.email], (err, res) => {
@@ -22,7 +26,19 @@ app.post('/api/users/login', async (request, response) => {
       if (res.rows[0]) {
         bcrypt.compare(request.body.password, res.rows[0].password, (err, result) => {
           if (result) {
-            response.send(true)
+            
+            const userToken = {
+              email: res.rows[0].email,
+              id: res.rows[0].id, 
+              name: res.rows[0].name
+            }
+            
+            const token = jwt.sign(userToken, process.env.SECRET)
+            
+            response.send({
+              status: true,
+              token
+            })
           }
           else {
             response.send(false)
