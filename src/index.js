a570dee6d2028384b0b826c204f0404c95551b61
Ethan Tyler <38672607+zonedv2@users.noreply.github.com>
2022-04-30 20:40:30 +0100

    change

--- src/index.js
+++ src/index.js
@@ -330,7 +330,7 @@ app.get('/api/stocks/analytics/friends/:date', auth, async (request, response) =
   response.send(friends)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
